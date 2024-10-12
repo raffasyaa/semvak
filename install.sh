fdf0bfe6d9ce0f3689ff2a17e11bb14a431cd505
raffasya <49897771+raffasyaa@users.noreply.github.com>
2024-10-12 03:15:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo -e "\e[32mStarting the installation process...\e[0m"
 
 # Download and move the script to /usr/local/bin
 echo -e "\e[33mDownloading and installing the backup script...\e[0m"
-curl -s -o /usr/local/bin/skt_backup https://raw.githubusercontent.com/raffasyaa/semvak/main/backup_marzban_v1.0.sh
+curl -s -o /usr/local/bin/skt_backup_v1 https://raw.githubusercontent.com/raffasyaa/semvak/main/backup_marzban_v1.0.sh
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
@@ -16,10 +16,10 @@ else
 fi
 
 # Grant executable permission to the script
-chmod +x /usr/local/bin/skt_backup
+chmod +x /usr/local/bin/skt_backup_v1
 # Display successful installation message
-echo -e "\e[32mInstallation complete. You can now use the command 'skt_backup' to run the script.\e[0m"
+echo -e "\e[32mInstallation complete. You can now use the command 'skt_backup_v1' to run the script.\e[0m"
 
 # Run the script
 echo -e "\e[33mRunning the script now...\e[0m"
-exec /usr/local/bin/skt_backup
+exec /usr/local/bin/skt_backup_v1
